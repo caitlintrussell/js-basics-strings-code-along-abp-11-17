1ab4f8d5aed2e35f8e31fef78f9e4c776d32eb24
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-08 03:29:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Caitlin';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser.toUpperCase()}`;
+const excitedWelcomeMessage = welcomeMessage.ToUpperCase();
